9b5b03030cb4586f7231f2577399821f202e88c4
kenshi kamata <kenshi.kamata@gmail.com>
2013-08-31 22:50:19 +0900

    update install.sh. you can choice setting files you want install

--- install.sh
+++ install.sh
@@ -1,4 +1,27 @@
 #!/bin/zsh
-for file in `find $HOME/dotfiles -name '.*' | grep -v 'dotfiles/.git$' | perl -nle 'm!dotfiles/(.+)$! and print $1'`; do
-    ln -is $HOME/dotfiles/$file $HOME/$file
+
+while getopts vtza option
+do
+    case $option in
+        "v" )
+            list=("vim" "vimrc")
+            ;;
+        "z" )
+            list=("zsh" "zshrc")
+            ;;
+	"t" )
+	    list=("tmux.conf")
+            ;;
+        "a" )
+            list=("tmux.conf" "vim" "vimrc" "zsh" "zshrc")
+    esac
+done
+
+for dotfile in ${list[@]}
+do
+    if [ -e ~/.$dotfile ]; then
+        echo "link to $dotfile"
+        rm -rf ~/.$dotfile
+    fi
+    ln -s `pwd`/$dotfile ~/.$dotfile
 done
